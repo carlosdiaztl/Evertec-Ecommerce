812c6ca67f93c78dfd45b36f81a32f0d335a32cd
carlosDiaz <diaz-alzate@hotmail.com>
2023-03-31 22:22:50 -0500

    test commit

--- tests/Feature/UserControllerIndexTest.php
+++ tests/Feature/UserControllerIndexTest.php
@@ -18,16 +18,24 @@ class UserControllerIndexTest extends TestCase
     /**
      * A basic feature test example.
      */
-    // public function test_Admin_can_access_AdminUsers()
-    // {
+    public function test_Admin_can_access_AdminHome()
+    {
 
-    //     // usuario con permiso de admin 
+        // usuario con permiso de admin 
 
-    //     // $user = User::find(51);
+        // $user = User::find(51);
 
+        $role1 =  Role::create(['name' => 'Admin']);
+        Permission::create(['name' => 'admin.users.index'])->assignRole($role1);
+        // Permission::create(['name' => 'admin.users.edit'])->assignRole($role1);
+        $user = User::factory()->create()->assignRole('Admin');
+        dump($user->hasRole('Admin') == true);
+        $response = $this->actingAs($user)->get('admin/home');
+        $response->assertStatus(200);
 
-    // }
-    public function test_User_cant_access_AdminUsers()
+        $response->assertSee('users');
+    }
+    public function test_User_cant_access_AdminHome()
     {
 
         $user = User::factory()->create();
@@ -36,16 +44,6 @@ class UserControllerIndexTest extends TestCase
         $response->assertStatus(403);
         // dump($response->getContent());
         $response->assertSee('This action is unauthorized.');
-
-        $role1 =  Role::create(['name' => 'Admin']);
-        Permission::create(['name' => 'admin.users.index'])->assignRole($role1);
-        // Permission::create(['name' => 'admin.users.edit'])->assignRole($role1);
-        $user = User::factory()->create()->assignRole('Admin');
-        dump($user->hasRole('Admin') == true);
-        $response = $this->actingAs($user)->get('/admin/home');
-        $response->assertStatus(200);
-
-        $response->assertSee('users');
     }
     public function  testIndex_Admin_search()
     // modificar este test para que el acceso sea a traves de la ruta y no la vista 
