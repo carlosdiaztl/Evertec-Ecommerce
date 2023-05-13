74f056e9c436033c9be26209b24925ff9dae8431
carlosDiaz <diaz-alzate@hotmail.com>
2023-05-13 13:02:02 -0500

    feat: add categories views create,update and deleetd

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::paginate();
+        $categories = Category::query()->paginate();
         return view('admin.categories.index', compact('categories'));
         //
     }
