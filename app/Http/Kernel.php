4485e67242367f5f4f52c8b6862e4b1a36ba123d
carlosdiaztl <diaz-alzate@hotmail.com>
2024-04-23 22:46:24 -0500

    secure https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\CheckUserStatus::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ForceHttps::class,
         ],
 
         'api' => [
